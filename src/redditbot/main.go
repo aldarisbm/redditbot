82e8bcb15e28d2c3a966b2c0f35d153fd3813c60
Jose Berrio <aldarisbm@gmail.com>
2020-07-02 23:36:15 -0500

    packages

--- src/redditbot/main.go
+++ src/redditbot/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
 
+	"github.com/turnage/graw/reddit"
+)
+
+func main() {
+	path, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("ERROR: Gettin WD %s", err)
+	}
+
+	bot, err := reddit.NewBotFromAgentFile(fmt.Sprintf("%s/secrets.agent", path), 0)
+	if err != nil {
+		log.Fatalf("ERROR: Retrieving Agent File: %s", err)
+	}
+
+	harvest, _ := bot.Listing("/r/golang", "")
+	for _, post := range harvest.Posts[:5] {
+		fmt.Printf("[%s] posted [%s]\n", post.Author, post.Title)
+	}
 }
