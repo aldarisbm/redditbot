81e9a9d930b22d3eaabea92903048c68dfe8d71e
Jose Berrio <aldarisbm@gmail.com>
2020-07-03 00:06:44 -0500

    posts

--- src/redditbot/main.go
+++ src/redditbot/main.go
@@ -14,13 +14,29 @@ func main() {
 		log.Fatalf("ERROR: Gettin WD %s", err)
 	}
 
+	// TODO: Implementation should change here and we should retrieve our secrets from secretsmanager
+
+	// cfg := BotConfig{
+	// 	Agent: AGENT
+	// 	App: App{
+	// 	  ID:     ID,
+	// 	  Secret: SECRET,
+	// 	  Username: USERNAME,
+	// 	  Password: PASSWORD,
+	// 	}
+	//   }
+
+	// bot, err := reddit.NewBot(cfg)
+
 	bot, err := reddit.NewBotFromAgentFile(fmt.Sprintf("%s/secrets.agent", path), 0)
 	if err != nil {
 		log.Fatalf("ERROR: Retrieving Agent File: %s", err)
 	}
 
-	harvest, _ := bot.Listing("/r/golang", "")
-	for _, post := range harvest.Posts[:5] {
-		fmt.Printf("[%s] posted [%s]\n", post.Author, post.Title)
+	submission, err := bot.GetPostSelf("LUC_team", "TEST GOLANG POST", "posting from bot")
+	if err != nil {
+		log.Fatalf("ERROR: Error while submitting the post -- %s", err)
 	}
+
+	fmt.Printf("%v", submission)
 }
